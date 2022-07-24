978a903c22b201da84338dee83c172cf2632f278
dev-shakib <mehedi@lskit.com>
2022-07-24 16:30:56 +0600

    solved link issues

--- storage/framework/views/490f3f5cb83129fee43d922616db8129c5046a1a.php
+++ storage/framework/views/490f3f5cb83129fee43d922616db8129c5046a1a.php
@@ -2,7 +2,7 @@
     <header class="custom-navbar">
         <nav class="navbar navbar-expand-lg">
             <div class="container">
-              <a class="navbar-brand text-shadow-1 fz-24 text-dark" href="#">BUNDLE-B</a>
+              <a class="navbar-brand text-shadow-1 fz-24 text-dark" href="<?php echo e(route('home')); ?>">BUNDLE-B</a>
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
               </button>
@@ -12,7 +12,7 @@
                     <a class="nav-link active" aria-current="page" href="<?php echo e(route('home')); ?>">Home</a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="/sorry">Link</a>
+                    <a class="nav-link" href="<?php echo e(route('login')); ?>">Link</a>
                   </li>
                 </ul><!-- navbar-nav./  -->
               </div><!-- collapse./  -->
