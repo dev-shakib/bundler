1367ff4a990949119323cf6ba91d977f56023f9d
dev-shakib <mehedi@lskit.com>
2022-08-15 11:01:10 +0600

    menu updated

--- resources/views/backend/partials/navbar.blade.php
+++ resources/views/backend/partials/navbar.blade.php
@@ -149,10 +149,49 @@
                     {{ Auth::user()->name }} <span class="caret"></span>
                 </a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item {{ Request::is('profile/' . Auth::user()->name, 'profile/' . Auth::user()->name . '/edit') ? 'active' : null }}"
+                    @role('admin')
+                    <a class="dropdown-item {{ Request::is('roles') || Request::is('permissions') ? 'active' : null }}"
+                        href="{{ route('laravelroles::roles.index') }}">
+                        {!! trans('titles.laravelroles') !!}
+                    </a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item {{ Request::is('users', 'users/' . Auth::user()->id, 'users/' . Auth::user()->id . '/edit') ? 'active' : null }}"
+                        href="{{ url('/users') }}">
+                        {!! trans('titles.adminUserList') !!}
+                    </a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item {{ Request::is('users/create') ? 'active' : null }}"
+                        href="{{ url('/users/create') }}">
+                        {!! trans('titles.adminNewUser') !!}
+                    </a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item {{ Request::is('package*') ? 'active' : null }}"
+                        href="{{ route('package.index') }}">
+                        {!! trans('titles.adminPackage') !!}
+                    </a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item {{ Request::is('setting*') ? 'active' : null }}"
+                        href="{{ route('setting.index') }}">
+                        Settings
+                    </a>
+                    @endrole
+                    @role('user')
+                    <a class="dropdown-item {{ Request::is('bundle*') ? 'active' : null }}"
                         href="{{ route('bundle.index') }}">
                         Bundle List
                     </a>
+                    @php
+                          $enrolled_package = auth()
+                              ->user()
+                              ->load('enrolledPackage')->enrolledPackage;
+                    @endphp
+                    @if ($enrolled_package->package_id == 3)
+                        <div class="dropdown-divider"></div>
+                        <a href="{{ route('setting.index') }}" class="dropdown-item {{ Request::is('setting*') ? 'active' : null }}">
+                            Settings
+                        </a>
+                    @endif
+                    @endrole
                     <div class="dropdown-divider"></div>
                     <a class="dropdown-item {{ Request::is('profile/' . Auth::user()->name, 'profile/' . Auth::user()->name . '/edit') ? 'active' : null }}"
                         href="{{ url('/profile/' . Auth::user()->name) }}">
